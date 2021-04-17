3d6ad1736863cd31a65e9138334cd27a6b14b97e
Hariharan Ragothaman <hariharanragothaman@gmail.com>
2021-04-17 13:27:28 -0400

    Some minor polish

--- problems/913/913B-christmas-spruce.py
+++ problems/913/913B-christmas-spruce.py
@@ -4,17 +4,13 @@ if __name__ == '__main__':
     vertices = int(input())
 
     # Tree formation
-    from collections import defaultdict, deque
+    from collections import defaultdict
     hmap = defaultdict(list)
     i = 1
     while i < vertices:
         parent = input()
         hmap[parent].append(str(i+1))
         i += 1
-    #print(hmap)
-
-    #q = deque(hmap)
-    #print(q)
 
     # Question is - every non-leaf vertex has atleast 3 leaf children
     for key, value in hmap.items():
